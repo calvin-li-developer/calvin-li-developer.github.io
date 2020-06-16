19a5b2332b089750209e644ce3e424d0ba6d36cb
Calvin Li <calvin.li.developer@gmail.com>
2020-06-16 00:22:52 -0400

    fix nav.js syntax for better code structure

--- js/nav.js
+++ js/nav.js
@@ -5,16 +5,16 @@ function navClick() {
     const navPanel = document.getElementById('navbtn-panel');
 
     if (navPanel.style.display != '') {
-        navPanel.style.display = ""
+        navPanel.style.display = "";
 
     } else {
         navPanel.style.display = "block";
     }
 }
 
-const myNav = document.getElementById("nav");
-
 window.onscroll = function () {
+    const myNav = document.getElementById("nav");
+
     if (document.documentElement.scrollTop > (document.getElementById('particles-js').clientHeight / 2) - (document.getElementById('full-name-header').clientHeight / 2) - document.getElementById('nav').clientHeight || document.body.scrollTop > (document.getElementById('particles-js').clientHeight / 2) - (document.getElementById('full-name-header').clientHeight / 2) - document.getElementById('nav').clientHeight) {
         myNav.classList.add("nav-background");
     } else {
