af315741c7fe43cbd714692d3993c1943bb46597
Calvin Li <calvin.li.developer@gmail.com>
2020-06-28 13:30:17 -0400

    clean up nav.js code

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,9 @@
-'use strict';
 
 function navClick() {
-    const burger = document.getElementById('burger');
+    'use strict';
     const navPanel = document.getElementById('navbtn-panel');
 
-    if (navPanel.style.display != '') {
+    if (navPanel.style.display !== '') {
         navPanel.style.display = "";
 
     } else {
@@ -13,6 +12,7 @@ function navClick() {
 }
 
 window.onscroll = function () {
+    'use strict';
     const myNav = document.getElementById("nav");
 
     if (document.documentElement.scrollTop > (document.getElementById('particles-js').clientHeight / 2) - (document.getElementById('full-name-header').clientHeight / 2) - document.getElementById('nav').clientHeight || document.body.scrollTop > (document.getElementById('particles-js').clientHeight / 2) - (document.getElementById('full-name-header').clientHeight / 2) - document.getElementById('nav').clientHeight) {
