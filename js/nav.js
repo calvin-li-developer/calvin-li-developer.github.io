8cb10b02f3fa860a79f3a6956abe5327415aaa21
Calvin Li <calvin.li.developer@gmail.com>
2020-07-26 11:24:32 -0400

    remove the use of for loops and better code formatting

--- js/nav.js
+++ js/nav.js
@@ -1,37 +1,47 @@
 function menuClickDisplay() {
-    "use strict";
-    const nav_menu_btns = document.querySelectorAll(".nav-menu-btn");
-    const nav_menu = document.querySelector(".nav-menu");
-    const menu_burger = document.querySelector(".menu-burger");
-    for (const button of nav_menu_btns) {
-        button.addEventListener("click", (e) => {
-            if (getComputedStyle(menu_burger, null).display === "block") {
-                nav_menu.style.display = "none";
-            }
-        });
-    }
+  "use strict";
+  const nav_menu_btns = document.querySelectorAll(".nav-menu-btn");
+  const nav_menu = document.querySelector(".nav-menu");
+  const menu_burger = document.querySelector(".menu-burger");
+  nav_menu_btns.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (getComputedStyle(menu_burger, null).display === "block") {
+        nav_menu.style.display = "none";
+      }
+    });
+  });
 }
 function onClickMenuBurger() {
-    "use strict";
-    document.querySelector(".menu-burger").addEventListener("click", () => {
-        const nav_menu = document.querySelector(".nav-menu");
-        if (nav_menu.style.display === "block") {
-            nav_menu.style.display = "";
-        } else {
-            nav_menu.style.display = "block";
-        }
-    });
+  "use strict";
+  document.querySelector(".menu-burger").addEventListener("click", () => {
+    const nav_menu = document.querySelector(".nav-menu");
+    if (nav_menu.style.display === "block") {
+      nav_menu.style.display = "";
+    } else {
+      nav_menu.style.display = "block";
+    }
+  });
 }
 window.onscroll = () => {
-    "use strict";
-    const nav_menu_bar = document.querySelector("nav");
+  "use strict";
+  const nav_menu_bar = document.querySelector("nav");
 
-    if (document.documentElement.scrollTop > (document.querySelector("#particles-js").clientHeight / 2) - (document.querySelector(".welcome").clientHeight) - (document.querySelector(".name").clientHeight) - document.querySelector("nav").clientHeight || document.body.scrollTop > (document.querySelector("#particles-js").clientHeight / 2) - (document.querySelector(".welcome").clientHeight) - (document.querySelector(".name").clientHeight) - document.querySelector("nav").clientHeight) {
-        nav_menu_bar.classList.add("nav-background");
-    } else {
-        nav_menu_bar.classList.remove("nav-background");
-    }
+  if (
+    document.documentElement.scrollTop >
+      document.querySelector("#particles-js").clientHeight / 2 -
+        document.querySelector(".welcome").clientHeight -
+        document.querySelector(".name").clientHeight -
+        document.querySelector("nav").clientHeight ||
+    document.body.scrollTop >
+      document.querySelector("#particles-js").clientHeight / 2 -
+        document.querySelector(".welcome").clientHeight -
+        document.querySelector(".name").clientHeight -
+        document.querySelector("nav").clientHeight
+  ) {
+    nav_menu_bar.classList.add("nav-background");
+  } else {
+    nav_menu_bar.classList.remove("nav-background");
+  }
 };
 menuClickDisplay();
 onClickMenuBurger();
-
